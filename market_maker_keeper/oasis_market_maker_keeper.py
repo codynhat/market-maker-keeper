9f572f6d47212b2ace6e58ccd8158f882a2c78e5
Cody Hatfield <cody.hatfield@me.com>
2020-04-10 10:17:54 -0700

    Fix oasis rpc_endpoint

--- market_maker_keeper/oasis_market_maker_keeper.py
+++ market_maker_keeper/oasis_market_maker_keeper.py
@@ -144,7 +144,7 @@ class OasisMarketMakerKeeper:
         self.arguments = parser.parse_args(args)
         setup_logging(self.arguments)
     
-        self.web3 = kwargs['web3'] if 'web3' in kwargs else Web3(HTTPProvider(endpoint_uri=f"http://{self.arguments.rpc_host}:{self.arguments.rpc_port}",
+        self.web3 = kwargs['web3'] if 'web3' in kwargs else Web3(HTTPProvider(endpoint_uri=f"{self.arguments.rpc_endpoint}",
                                                                               request_kwargs={"timeout": self.arguments.rpc_timeout}))
         self.web3.eth.defaultAccount = self.arguments.eth_from
         register_keys(self.web3, self.arguments.eth_key)
