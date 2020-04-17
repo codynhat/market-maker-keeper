457136ef9b1c0b06b8c13a2e0410aee6cf61d671
Cody Hatfield <cody.hatfield@me.com>
2020-04-17 11:15:27 -0700

    Update smart gas strategy

--- market_maker_keeper/gas.py
+++ market_maker_keeper/gas.py
@@ -35,18 +35,18 @@ class SmartGasPrice(GasPrice):
         self.gas_station = EthGasStation(refresh_interval=60, expiry=600, api_key=api_key)
 
     def get_gas_price(self, time_elapsed: int) -> Optional[int]:
-        fast_price = self.gas_station.fast_price()
-        if fast_price is not None:
-            # start from fast_price + 10 GWei
+        standard_price = self.gas_station.standard_price()
+        if standard_price is not None:
+            # start from standard_price
             # increase by 10 GWei every 60 seconds
-            # max is fast_price + 50 GWei
-            return min(int(fast_price*1.1) + int(time_elapsed/60)*(10*self.GWEI), int(fast_price*1.1)+(50*self.GWEI))
+            # max is standard_price + 50 GWei
+            return min(int(standard_price) + int(time_elapsed/60)*(10*self.GWEI), int(standard_price)+(50*self.GWEI))
         else:
             # default gas pricing when EthGasStation feed is down
-            return IncreasingGasPrice(initial_price=20*self.GWEI,
+            return IncreasingGasPrice(initial_price=5*self.GWEI,
                                       increase_by=10*self.GWEI,
                                       every_secs=60,
-                                      max_price=100*self.GWEI).get_gas_price(time_elapsed)
+                                      max_price=50*self.GWEI).get_gas_price(time_elapsed)
 
 
 class GasPriceFactory:
