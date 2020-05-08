f2a87b323de733dcc87f91f7e6874265d1a7ceb6
Cody Hatfield <cody.hatfield@me.com>
2020-05-08 09:40:59 -0700

    Fix precision

--- market_maker_keeper/coinbase_market_maker_keeper.py
+++ market_maker_keeper/coinbase_market_maker_keeper.py
@@ -133,7 +133,7 @@ class CoinbaseMarketMakerKeeper:
         # Get maximum number of decimals for prices and amounts.
         quote_increment = self.coinbase_api.get_product(self.arguments.pair)["quote_increment"]
         self.logger.debug(f"Quote increment: {quote_increment}")
-        self.quote_increment = -(int(log10(float(quote_increment)))+1)
+        self.quote_increment = max(2, -(int(log10(float(quote_increment)))+1))
         self.logger.debug(f"Quote precision: {self.quote_increment}")
 
         currencies = self.coinbase_api._http_unauthenticated("GET", f"/currencies", {})
